ef7efde2946df8d19763f5d9f22eec6976b591bb
humer123 <120168181+humer123@users.noreply.github.com>
2023-10-04 00:55:36 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/client/index.html");
+})
+app.get("/login", (req, res) =>{
+    res.sendFile(__dirname + "/client/login.html");
+})
+app.get("/register", (req, res) =>{
+    res.sendFile(__dirname + "/client/register.html");
+})
+
+io.on("connection", (socket) =>{
+
+    console.log("connected");
+
+    socket.on("chat message", (message) =>{
+
+        io.emit("chat message", message);
+
+    });
+
+    socket.on("disconnect", ()=>{
+        console.log("disconnected");
+
+
+    });
+
+
+});
+
+
+server.listen(4000, () =>{
+    console.log("Server listening on PORT 4000");
+});
+
+
